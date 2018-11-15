8360b018c661190b7476a6d8546cb5c3c310a8ec
grzeli <grzelak.szymon@gmail.com>
2018-11-15 23:15:28 +0100

    menu

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -14,6 +14,9 @@ $(document).ready(function($) {
   $('.mobile-menu-messages').click(function(){
     $('.more-messages').toggle()
   });
+  $('#log-in').click(function(){
+    $('.desktop-user-menu').toggle()
+  });
   var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
     var hamburgers = document.querySelectorAll(".hamburger");
     if (hamburgers.length > 0) {
