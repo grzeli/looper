0a0907ffc9d46005bd9b27b2f2ec56809ee0ffed
grzeli <grzelak.szymon@gmail.com>
2019-01-12 21:46:56 +0100

    search results mobile version

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,9 @@ $(document).ready(function($) {
   $('.filter-results').click(function() {
     $('.search-results_filters').toggle()
   });
+  $('.search-filters_close-icon').click(function(){
+    $('.search-results_filters').hide()
+  });
 });
 $(document).ready(function($) {  
   $('#nav-toggle').click(function() {
--- src/js/filters.js
+++ src/js/filters.js
@@ -2,4 +2,7 @@ $(document).ready(function($) {
   $('.filter-results').click(function() {
     $('.search-results_filters').toggle()
   });
+  $('.search-filters_close-icon').click(function(){
+    $('.search-results_filters').hide()
+  });
 });
