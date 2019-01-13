14720aa63d6e5b8ee51f0b26507c71cd99cb53f9
grzeli <grzelak.szymon@gmail.com>
2019-01-13 18:12:05 +0100

    search results desktop | Done

--- app/js/main.js
+++ app/js/main.js
@@ -37,6 +37,7 @@ $(document).ready(function($) {
   $('#log-in').click(function(){
     $('.desktop-user-menu').toggle()
     $('.btn.btn-yellow.add-trip').toggleClass('active')
+    $('.user-menu_container').toggleClass('active')
   });
   var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
     var hamburgers = document.querySelectorAll(".hamburger");
